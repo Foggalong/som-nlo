aedd8cc52c154e33ba2519454a8933cc909a3a89
Josh Fogg <jfogg@duck.com>
2025-08-08 11:27:04 +0100

    Made `update` a parameter of `QNDirection` The functions are the same across Labs 3, 4, and 5, excepting the defaults for `update` and `output`.

--- Lab4/QNDirection.py
+++ Lab4/QNDirection.py
@@ -1,31 +1,22 @@
 import numpy as np
 
-# update = "SR1"
-# update = "DFP"
-update = "BFGS"
 
-
-def QNDirection(Hk, xkp, xk, gkp, gk, output=True):
+def QNDirection(Hk, xkp, xk, gkp, gk, update="BFGS", output=True):
     """
-    Quasi-Newton Direction:
-        calculates the next direction for Quasi-Newton methods
-
-    - It uses a variant that updates H (by default)
-
-    - it needs to be passed the previous H (and also yk and delta_k)
-
-    - It supports SR1, DFP and BFGS
-
-    - it can be linked with the generic line search method and
-    different line searches
-
-    Called as dk, Hkp =  CGDQuasiNewton(Hk, xk, xkp, gk, gkp), where
+    Calculates the next direction for Quasi-Newton methods, where
     - Hk is old (inverse) Hessian approximation
     - xkp, xk are the previous two iterates
     - gkp, gk are the previous two gradients
+
     It returns
     - dkp: next search direction dkp = -Hkp gkp
     - Hkp: the new Hessian approximation
+
+    Takes an optional string `update` to control the method for updating H. It
+    supports `SR1`, `DFP` and `BFGS` (default).
+
+    Takes an optional boolean `output` (default `True`) which controls
+    whether to print progress.
     """
 
     if output:
@@ -34,39 +25,36 @@ def QNDirection(Hk, xkp, xk, gkp, gk, output=True):
     delta = xkp - xk
     y = gkp - gk
 
-    # check that the QN-curvature condition is satisfied
-    # (this really has to be the case by the Wolfe conditions which
-    #  are enforced by the Bisection Linesearch).
+    # Double check that the QN-curvature condition is satisfied. This should
+    # be the case by the Wolfe conditions, which are enforced by the Bisection
+    # Linesearch.
     yd = y.dot(delta)
     if yd <= 0:
-        raise ValueError("QN-curvature condition not satisfied: STOP!")
+        raise ValueError("QN-curvature condition not satisfied!")
 
-    # Hkp = Hk.copy();
+    # This is required by all three update methods, so save it to a variable
+    # to make calculations more efficient.
+    Hy = Hk.dot(y)
+    Hkp = Hk.copy()
 
     if update == "SR1":
-        # The symmetric SR1 update (H version)
+        # The symmetric SR1 update equation is (H version)
         # H+ = H + (delta-H*y)*(delta-H*y)'/((delta-H*y)'y)
-        Hy = Hk.dot(y)
-        dH = np.outer(delta-Hy, delta-Hy)
-        fac = y.dot(delta-Hy)
-        dH = dH/fac
-        Hkp = Hk + dH
+        Hkp += np.outer(delta-Hy, delta-Hy)/y.dot(delta-Hy)
 
     elif update == "DFP":
-        # The DFP update
+        # The DFP update equation is
         # H+ = H + dd^T/d^Ty - Hy(Hy)'/y'Hy
-        Hy = Hk.dot(y)
-        Hkp = Hk + np.outer(delta, delta)/yd - np.outer(Hy, Hy)/(Hy.dot(y))
+        Hkp += np.outer(delta, delta)/yd - np.outer(Hy, Hy)/(Hy.dot(y))
 
     elif update == "BFGS":
-        # The BFGS update
+        # The BFGS update equation is
         # H+ = H + (1+y'Hy/y'd)*(dd')/y'd - (dy'H' + Hyd')/y'd
-        Hy = Hk.dot(y)  # remember this to make calculations more efficient
-
-        Hkp = Hk + (1+y.dot(Hy)/yd)*(np.outer(delta, delta))/yd - (np.outer(delta, Hy)+np.outer(Hy, delta))/yd
+        Hkp += (1 + y.dot(Hy)/yd)*(np.outer(delta, delta))/yd - \
+               (np.outer(delta, Hy) + np.outer(Hy, delta))/yd
 
     else:
-        raise ValueError("did not recognise update")
+        raise ValueError("Update method not recognised")
 
     if output:
         print(f"QN inverse Hessian approx is:\n{Hkp}")
